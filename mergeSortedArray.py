f7ae31ff31847066d98c94e56cd7b30dc03565db
Rohit Raj <138340876+roh8raj@users.noreply.github.com>
2025-04-19 17:01:32 +0530

    mergeTwoSortedArray

---
+++ mergeSortedArray.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        l=(m+n)-1
+        m=m-1
+        n=n-1
+        
+
+        while m>=0 and n>=0:
+            if nums1[m]>nums2[n]:
+                nums1[l]=nums1[m]
+                m=m-1
+                
+            else:
+                nums1[l]=nums2[n]
+                n=n-1
+
+            l=l-1            
+
+        while n>=0:
+            nums1[l]=nums2[n]
+            n=n-1
+            l=l-1
+
+        
+        
+       
+            
+
+       
+        
